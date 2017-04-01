993763f514a188a40bf132b964a743c7dcbc12b4
IvanLin <lyfne@me.com>
2017-04-01 11:52:24 +0800

    Initial Commit

---
+++ Sudoku/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sudoku
+//
+//  Created by 林逸凡 on 2017/4/1.
+//  Copyright © 2017年 Ivan.lin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by 林逸凡 on 2017/4/1.
+//  Copyright © 2017年 Ivan.lin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
